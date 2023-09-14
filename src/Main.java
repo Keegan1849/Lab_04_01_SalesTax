c1692d4d2ad2778393cef936b5a7f85b4e98d473
[Keegan Chapin] <[keeg.chapin@gmail.com]>
2023-09-14 13:36:28 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double price = 17.99;
+        double finalPrice = 0.0;
+        finalPrice = price * 1.05;
+
+
+        System.out.println("The final price is " + finalPrice);
+    }
+}
